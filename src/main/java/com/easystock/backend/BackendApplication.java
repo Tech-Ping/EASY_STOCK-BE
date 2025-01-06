3081d139b555c04fa924157981344f5863184444
베뉴 <christer10@ewhain.net>
2025-01-06 14:43:39 +0900

    init: add basic structures for initial settings

---
+++ src/main/java/com/easystock/backend/BackendApplication.java
@@ -0,0 +1,20 @@
+package com.easystock.backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
