870df2ff12eeb91deada9ef83ea070fe54ba9e40
베뉴 <christer10@ewhain.net>
2025-01-06 14:43:39 +0900

    init: add basic structures for initial settings

---
+++ src/test/java/com/easystock/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easystock.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
