cd615ba0578932835f3b6edfedc7b0e0c5c331e0
베뉴 <christer10@ewhain.net>
2025-01-11 20:29:21 +0900

    feat: 회원 도메인 엔티티 정의
    - `Member` 엔티티 정의
    - `LevelType` enum 정의

---
+++ src/main/java/com/easystock/backend/infrastructure/database/entity/Member.java
@@ -0,0 +1,38 @@
+package com.easystock.backend.infrastructure.database.entity;
+
+import com.easystock.backend.infrastructure.database.entity.common.AuditingEntity;
+import com.easystock.backend.infrastructure.database.entity.enums.LevelType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+@Table(name = "MEMBER")
+public class Member extends AuditingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "username",nullable = false, length = 20)
+    private String username;
+
+    @Column(name= "password", nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "birth_date",nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "level", nullable = false)
+    private LevelType level;
+
+    @Column(name = "is_agreed", nullable = false)
+    private Boolean isAgreed;
+
+}
