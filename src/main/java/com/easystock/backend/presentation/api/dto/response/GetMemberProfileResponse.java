9ffe11cc15fb33383137a8d439d679a8c18bda5a
베뉴 <christer10@ewhain.net>
2025-01-15 03:04:54 +0900

    feat: 회원 프로필 정보 반환 dto 정의 (#9)

--- src/main/java/com/easystock/backend/presentation/api/dto/response/GetMemberProfileResponse.java
+++ src/main/java/com/easystock/backend/presentation/api/dto/response/GetMemberProfileResponse.java
@@ -1,2 +1,21 @@
-package com.easystock.backend.presentation.api.dto.response;public class GetMemberProfileResponse {
+package com.easystock.backend.presentation.api.dto.response;
+
+import com.easystock.backend.infrastructure.database.entity.enums.LevelType;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class GetMemberProfileResponse {
+    @Schema(description = "회원의 프로필 이미지")
+    private int profileImage;
+
+    @Schema(description = "회원의 레벨")
+    private LevelType level;
+
+    @Schema(description = "회원의 보유 STOKEN")
+    private int tokenBudget;
+
+    @Schema(description = "회원의 닉네임")
+    private String nickname;
+
+    @Schema(description = "회원의 경험치 XP")
+    private int xpGuage;
 }
