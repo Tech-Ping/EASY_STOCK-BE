08fae19636d26a70fde9ea58d2c539e5940c0a9a
nzeong <stac_cato@naver.com>
2025-01-16 21:33:03 +0900

    fix: 주문 엔티티명 Order에서 Trade로 변경 (#12)
    - order는 SQL 예약어이기 때문에 엔티티명으로 사용 불가능

--- src/main/java/com/easystock/backend/infrastructure/database/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.easystock.backend.infrastructure.database.repository;
-
-import com.easystock.backend.infrastructure.database.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
