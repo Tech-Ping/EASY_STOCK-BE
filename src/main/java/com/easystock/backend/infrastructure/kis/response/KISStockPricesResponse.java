9e88fa983e1af63248e8d6df3d43fe1998ffe3ea
nzeong <stac_cato@naver.com>
2025-01-17 01:52:57 +0900

    feat: 한투 주식 현재가 시세 API의 Response 받아오는 dto 추가 (#15)

---
+++ src/main/java/com/easystock/backend/infrastructure/kis/response/KISStockPricesResponse.java
@@ -0,0 +1,22 @@
+package com.easystock.backend.infrastructure.kis.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class KISStockPricesResponse {
+
+    @JsonProperty("output")
+    private final KISStockPricesOutputResponse output;
+
+    @JsonProperty("rt_cd")
+    private final String rtCd;
+
+    @JsonProperty("msg_cd")
+    private final String msgCd;
+
+    @JsonProperty("msg1")
+    private final String msg1;
+}
