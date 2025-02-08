dbcab0802c3be41fd614846b62bdb3cf68d556b0
Yunjeong Lee <121355994+nzeong@users.noreply.github.com>
2025-02-08 20:59:55 +0900

    [Feat] 주식 호가, 잔량 조회 API 추가 (#33)
    * chore: DB column 이름 수정 (#16)
    
    * feat: 거래 목록 조회 API 구현 (#16)
    
    - 로그인한 사용자의 모든 거래를 반환하는 API 구현
    - 로그인한 사용자의 특정 상태(거래 완료, 취소, 대기)에 따른 거래 목록을 반환하는 API 구현
    
    * feat: StockException 추가 (#24)
    
    * feat: 주식 세부 정보 조회 API 구현 (#24)
    
    - 특정 주식의 기본 정보 및 종목명, 현재가, 등락, 등락률 정보를 반환
    - 중복되는 코드 getStockPriceFromApi 메소드로 분리
    
    * feat: 한투 Open API에서 주식 호가/잔량 받아오는 response 추가 (#26)
    
    * refact: exchangeRestTemplate, getVoidHttpEntity 재사용 가능하도록 리팩토링 (#26)
    
    * feat: 주식 호가/잔량 조회 API 구현 (#26)
    
    - 주식 ID, 매도/매수를 기반으로 특정 주식의 실시간 호가와 잔량을 각각 10개씩 조회하는 API
    
    * chore: 주식 호가/잔량 조회 API 엔트포인트 변경 (#26)

---
+++ src/main/java/com/easystock/backend/infrastructure/kis/response/KisStockQuotesOutput1Response.java
@@ -0,0 +1,129 @@
+package com.easystock.backend.infrastructure.kis.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class KisStockQuotesOutput1Response {
+    @JsonProperty("askp1")
+    private final String askp1;
+
+    @JsonProperty("askp2")
+    private final String askp2;
+
+    @JsonProperty("askp3")
+    private final String askp3;
+
+    @JsonProperty("askp4")
+    private final String askp4;
+
+    @JsonProperty("askp5")
+    private final String askp5;
+
+    @JsonProperty("askp6")
+    private final String askp6;
+
+    @JsonProperty("askp7")
+    private final String askp7;
+
+    @JsonProperty("askp8")
+    private final String askp8;
+
+    @JsonProperty("askp9")
+    private final String askp9;
+
+    @JsonProperty("askp10")
+    private final String askp10;
+
+    @JsonProperty("bidp1")
+    private final String bidp1;
+
+    @JsonProperty("bidp2")
+    private final String bidp2;
+
+    @JsonProperty("bidp3")
+    private final String bidp3;
+
+    @JsonProperty("bidp4")
+    private final String bidp4;
+
+    @JsonProperty("bidp5")
+    private final String bidp5;
+
+    @JsonProperty("bidp6")
+    private final String bidp6;
+
+    @JsonProperty("bidp7")
+    private final String bidp7;
+
+    @JsonProperty("bidp8")
+    private final String bidp8;
+
+    @JsonProperty("bidp9")
+    private final String bidp9;
+
+    @JsonProperty("bidp10")
+    private final String bidp10;
+
+    @JsonProperty("askp_rsqn1")
+    private final String askpRsqn1;
+
+    @JsonProperty("askp_rsqn2")
+    private final String askpRsqn2;
+
+    @JsonProperty("askp_rsqn3")
+    private final String askpRsqn3;
+
+    @JsonProperty("askp_rsqn4")
+    private final String askpRsqn4;
+
+    @JsonProperty("askp_rsqn5")
+    private final String askpRsqn5;
+
+    @JsonProperty("askp_rsqn6")
+    private final String askpRsqn6;
+
+    @JsonProperty("askp_rsqn7")
+    private final String askpRsqn7;
+
+    @JsonProperty("askp_rsqn8")
+    private final String askpRsqn8;
+
+    @JsonProperty("askp_rsqn9")
+    private final String askpRsqn9;
+
+    @JsonProperty("askp_rsqn10")
+    private final String askpRsqn10;
+
+    @JsonProperty("bidp_rsqn1")
+    private final String bidpRsqn1;
+
+    @JsonProperty("bidp_rsqn2")
+    private final String bidpRsqn2;
+
+    @JsonProperty("bidp_rsqn3")
+    private final String bidpRsqn3;
+
+    @JsonProperty("bidp_rsqn4")
+    private final String bidpRsqn4;
+
+    @JsonProperty("bidp_rsqn5")
+    private final String bidpRsqn5;
+
+    @JsonProperty("bidp_rsqn6")
+    private final String bidpRsqn6;
+
+    @JsonProperty("bidp_rsqn7")
+    private final String bidpRsqn7;
+
+    @JsonProperty("bidp_rsqn8")
+    private final String bidpRsqn8;
+
+    @JsonProperty("bidp_rsqn9")
+    private final String bidpRsqn9;
+
+    @JsonProperty("bidp_rsqn10")
+    private final String bidpRsqn10;
+}
